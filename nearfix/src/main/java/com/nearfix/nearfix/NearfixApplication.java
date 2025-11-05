47f261359bf2737e0db0ccb598e0fe39b9237975
Rohith <gredyrohith21@gmail.com>
2025-11-05 12:16:30 +0530

    Project setup

---
+++ nearfix/src/main/java/com/nearfix/nearfix/NearfixApplication.java
@@ -0,0 +1,13 @@
+package com.nearfix.nearfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearfixApplication.class, args);
+	}
+
+}
---
+++ nearfix/src/test/java/com/nearfix/nearfix/NearfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearfix.nearfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
